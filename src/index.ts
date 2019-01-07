d2611dd8e86b11ba929ccf6d9881a36566c8f740
Vladimir <vzh.inbox@gmail.com>
2019-01-07 21:20:17 +0600

    Fix Omit for inherited types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-export type Omit<
-  A extends object,
-  K extends Extract<K, K> extends keyof A
-    ? keyof A
-    : (Extract<keyof A, keyof K> extends never ? never : Pick<K, Extract<keyof A, keyof K>>)
-> = Pick<A, Exclude<keyof A, K extends keyof A ? K : keyof K>>;
+export type Copy<A extends object> = { [P in keyof A]: A[P] };
 
 export type DiffKeys<A extends object, B extends object> = Exclude<keyof A, keyof B>;
 
@@ -15,8 +10,22 @@ export type Intersection<A extends object, B extends object> = Pick<A, Intersect
 
 export type Merge<A extends object, B extends object> = Diff<A, B> & B;
 
-export type Overwrite<
+// export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// export type Omit<
+//   A extends object,
+//   K extends Extract<K, K> extends keyof A
+//     ? keyof A
+//     : (Extract<keyof A, keyof K> extends never ? never : object)
+// > = Pick<A, Exclude<keyof A, K extends keyof A ? K : keyof K>>;
+export type Omit<
   A extends object,
+  K extends
+    | keyof A
+    | (Extract<keyof A, keyof K> extends never ? never : Pick<K, Extract<keyof A, keyof K>>)
+> = Pick<A, Exclude<keyof A, K extends keyof A ? K : keyof K>>;
+
+export type Overwrite<
+  A extends object | B,
   B extends IntersectionKeys<B, A> extends never ? never : Intersection<B, A>
 > = { [P in keyof Merge<A, B>]: Merge<A, B>[P] };
 
@@ -25,6 +34,6 @@ export type KeysOfType<A extends object, B> = {
   [P in keyof A]: A[P] extends B ? P : never
 }[keyof A];
 
-export type ExcludeKeys<A extends object, B> = Pick<A, Exclude<keyof A, KeysOfType<A, B>>>;
+export type ExcludeKeysOfType<A extends object, B> = Pick<A, Exclude<keyof A, KeysOfType<A, B>>>;
 
-export type ExtractKeys<A extends object, B> = Pick<A, KeysOfType<A, B>>;
+export type ExtractKeysOfType<A extends object, B> = Pick<A, KeysOfType<A, B>>;
--- src/test.ts
+++ src/test.ts
@@ -4,12 +4,19 @@ type A = { a: number; c: string }; // $ExpectType "F"
 type B = { a?: boolean; b: string };
 type D = { d: number; e: string };
 
+interface I extends A {
+  i: number;
+}
+
 type Omit1 = Omit<B, 'a'>; // OK
 type Omit2 = Omit<B, 'a' | 'c'>; // Error: `c` not in `B`
 type Omit3 = Omit<B, keyof A>; // Error
 type Omit4 = Omit<B, A>; // OK: Exclude all intersections with A
 type Omit5 = Omit<B, A & D>; // OK: Exclude all intersections
 type Omit6 = Omit<B, D>; // Error: `D` has no any intersections with `B`, so `Omit` is unnecessary.
+type Omit7<I extends object, P extends I> = Omit<P, keyof I>; // OK
+type Omit8<I extends object, P extends I> = Omit<P, I>; // Error
+type Omit9<I extends object, P extends object> = Omit<P, keyof I>; // Error
 
 type Overwrite1 = Overwrite<A, B>;
 type Overwrite2 = Overwrite<A, D>;
