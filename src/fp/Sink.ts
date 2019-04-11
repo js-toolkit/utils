9ac9d80095c185f8f4cd4771a32340e35b8e3faf
Vladimir <vzh.inbox@gmail.com>
2019-04-11 20:39:38 +0600

    Add Sink

---
+++ src/fp/Sink.ts
@@ -0,0 +1,64 @@
+export default class Sink<A> {
+  private readonly promise: Promise<never>;
+
+  private isPending: boolean;
+
+  private waitTimeoutHandler?: any;
+
+  private resolve?: () => void;
+
+  private reject?: (reason?: any) => void;
+
+  private finalizator?: (() => void) | void;
+
+  private pipeHandler?: (value: any) => any;
+
+  private onPipe = (value: any): any => {
+    try {
+      this.pipeHandler && this.pipeHandler(value);
+    } catch (ex) {
+      this.cancel(ex);
+    }
+  };
+
+  constructor(
+    executor: (pipe: <B>(value: A) => B, cancel: Sink<A>['cancel']) => (() => void) | void
+  ) {
+    this.cancel = this.cancel.bind(this);
+    this.wait = this.wait.bind(this);
+    this.pipe = this.pipe.bind(this);
+
+    this.isPending = true;
+    this.promise = new Promise<never>((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+      this.finalizator = executor(this.onPipe, this.cancel);
+    }).finally(() => {
+      this.isPending = false;
+      clearTimeout(this.waitTimeoutHandler);
+    });
+  }
+
+  wait(timeout?: number): Promise<never> {
+    if (this.isPending && timeout && timeout > 0) {
+      this.waitTimeoutHandler = setTimeout(() => this.cancel(), timeout);
+    }
+    return this.promise;
+  }
+
+  cancel(reason?: any): void {
+    if (!this.resolve || !this.reject) throw new Error('Invalid operation.');
+    this.finalizator && this.finalizator();
+    if (reason) {
+      this.reject && this.reject(reason);
+    } else {
+      this.resolve && this.resolve();
+    }
+  }
+
+  pipe<B>(callback: (value: A) => B): Sink<B> {
+    const prevHandler = this.pipeHandler;
+    this.pipeHandler = value => callback(prevHandler ? prevHandler(value) : value);
+    return this as any;
+  }
+}
--- src/fp/index.ts
+++ src/fp/index.ts
@@ -1,3 +1,5 @@
 export * from './errors';
 export * from './Option';
 export * from './Try';
+export { default as Sink } from './Sink';
+export * from './Sink';
