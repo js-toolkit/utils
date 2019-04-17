734710400634e726dd3b7df27a8e8f61acbdb126
Vladimir <vzh.inbox@gmail.com>
2019-04-17 14:12:20 +0600

    Add catch

--- src/fp/Sink.ts
+++ src/fp/Sink.ts
@@ -3,7 +3,11 @@ type Finalizator = () => void | Promise<void>;
 export default class Sink<A> {
   private readonly promise: Promise<never>;
 
+  private readonly cancelOnError: boolean;
+
   private pending: boolean = true;
+
+  private cancelling: boolean = false;
 
   private waitTimeoutHandler?: any;
 
@@ -15,20 +19,16 @@ export default class Sink<A> {
 
   private pipeHandler?: (value: any) => Promise<any>;
 
-  private onPipe = async (value: any): Promise<void> => {
-    try {
-      this.pipeHandler && (await this.pipeHandler(value));
-    } catch (ex) {
-      this.cancel(ex);
-    }
-  };
-
   constructor(
-    executor: (pipe: (value: A) => any, cancel: Sink<A>['cancel']) => Finalizator | void
+    executor: (pipe: (value: A) => any, cancel: Sink<A>['cancel']) => Finalizator | void,
+    cancelOnPipeError: boolean = true
   ) {
     this.cancel = this.cancel.bind(this);
     this.wait = this.wait.bind(this);
     this.pipe = this.pipe.bind(this);
+    this.onPipe = this.onPipe.bind(this);
+
+    this.cancelOnError = cancelOnPipeError;
 
     this.promise = new Promise<never>((resolve, reject) => {
       this.resolve = resolve;
@@ -54,14 +54,29 @@ export default class Sink<A> {
 
   cancel(reason?: any): Promise<void> {
     if (!this.isPending) return Promise.resolve();
+    if (this.cancelling) return Promise.reject(new Error('Already in cancelling state.'));
+
+    this.cancelling = true;
+    // finalizator may works long time so cancel timer because already in cancelling state.
+    clearTimeout(this.waitTimeoutHandler);
+
     return Promise.resolve()
       .then(this.finalizator)
-      .then(() => {
+      .catch(ex => {
+        if (reason) console.error('Cancel error:', ex);
+        else throw ex;
+      })
+      .finally(() => {
         if (reason) {
           this.reject && this.reject(reason);
         } else {
           this.resolve && this.resolve();
         }
+        this.cancelling = false;
+        this.finalizator = undefined;
+        this.reject = undefined;
+        this.resolve = undefined;
+        this.pipeHandler = undefined;
       });
     // if (!this.isPending) return;
     // this.finalizator && (await this.finalizator());
@@ -72,10 +87,21 @@ export default class Sink<A> {
     // }
   }
 
+  private onPipe(value: any): void {
+    this.pipeHandler && this.pipeHandler(value).catch(this.cancelOnError ? this.cancel : undefined);
+  }
+
   pipe<B>(action: (value: A) => PromiseLike<B> | B): Sink<B> {
     const prevHandler = this.pipeHandler;
     this.pipeHandler = value =>
       (prevHandler ? prevHandler(value) : Promise.resolve(value)).then(action);
+    return this as any;
+  }
+
+  catch<B = never>(action: (reason: any) => PromiseLike<B> | B): Sink<B> {
+    const prevHandler = this.pipeHandler;
+    this.pipeHandler = value =>
+      (prevHandler ? prevHandler(value) : Promise.resolve(value)).catch(action);
     return this as any;
   }
 }
