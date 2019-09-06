f85ebd8fee71cd77a8c604e5d31b62bc9125cbe5
Vladimir <vzh.inbox@gmail.com>
2019-09-06 15:02:10 +0600

    Strict Overwrite

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,6 @@ export type Intersection<A extends object, B extends object> = Pick<A, Intersect
 
 export type Merge<A extends object, B extends object> = Diff<A, B> & B;
 
-// export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-// export type Omit<
-//   A extends object,
-//   K extends Extract<K, K> extends keyof A
-//     ? keyof A
-//     : (Extract<keyof A, keyof K> extends never ? never : object)
-// > = Pick<A, Exclude<keyof A, K extends keyof A ? K : keyof K>>;
 export type Omit<
   A extends object,
   K extends
@@ -26,12 +19,12 @@ export type Omit<
 
 export type Overwrite<
   A extends object,
-  B extends IntersectionKeys<B, A> extends never ? never : Intersection<B, A>
+  B extends DiffKeys<B, A> extends never ? Intersection<B, A> : never
 > = { [P in keyof Merge<A, B>]: Merge<A, B>[P] };
 
 /** Return keys of type `T` */
 export type KeysOfType<A extends object, B> = {
-  [P in keyof A]: A[P] extends B ? P : never
+  [P in keyof A]: A[P] extends B ? P : never;
 }[keyof A];
 
 export type ExcludeKeysOfType<A extends object, B> = Pick<A, Exclude<keyof A, KeysOfType<A, B>>>;
