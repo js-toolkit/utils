ccff8b2765719635f78b81ff6b17fd40f00dcf61
Vladimir <vzh.inbox@gmail.com>
2020-06-02 11:58:52 +0600

    Update deps and configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: require.resolve('@vzh/configs/eslint/ts.common.eslintrc.js'),
+  extends: require.resolve('@vzh/configs/eslint/common'),
   rules: {
     'import/export': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
