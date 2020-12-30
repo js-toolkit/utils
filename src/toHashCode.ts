7983e6c38ffc9a783532de1e130cebb0d30e681d
Vladimir <vzh.inbox@gmail.com>
2020-12-30 14:20:46 +0600

    Add toHashCode

---
+++ src/toHashCode.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-bitwise */
+
+/**
+ * Javascript implementation of java's string hashcode method.
+ * @see https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+export default function toHashCode(str: string): number {
+  let hash = 0;
+  if (str.length === 0) return hash;
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash &= hash; // Convert to 32bit integer
+  }
+
+  return hash;
+}
