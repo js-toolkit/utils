7344b8573643f813777c7cea47942f635b05eb4c
Vladimir <vzh.inbox@gmail.com>
2021-01-12 11:41:28 +0600

    Add context

--- src/beforeCall.ts
+++ src/beforeCall.ts
@@ -1,10 +1,11 @@
 export default function beforeCall<F extends AnyFunction>(
   fn: F,
-  callback: (...args: Parameters<F>) => void
+  callback: (...args: Parameters<F>) => void,
+  context: any = undefined
 ): F {
   const wrapper: AnyFunction = (...args: Parameters<F>) => {
     callback(...args);
-    return fn.call(undefined, ...args) as ReturnType<F>;
+    return fn.call(context, ...args) as ReturnType<F>;
   };
 
   const descs = Object.getOwnPropertyDescriptors(fn);
