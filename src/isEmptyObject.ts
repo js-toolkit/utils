90dce0ade21514346649589a0a37916d0b0bfef5
Vladimir <vzh.inbox@gmail.com>
2021-02-03 11:45:49 +0600

    Add isEmptyObject

---
+++ src/isEmptyObject.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable guard-for-in */
+
+export default function isEmptyObject<T extends AnyObject>(obj: T): boolean {
+  for (const _ in obj) {
+    return false;
+  }
+  return true;
+}
