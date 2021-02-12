efc1f0e299d9a0b4762c0eba7ba540858d311f1f
Vladimir <vzh.inbox@gmail.com>
2021-02-12 12:30:22 +0600

    Improve getErrorMessage

--- src/getErrorMessage.ts
+++ src/getErrorMessage.ts
@@ -1,6 +1,10 @@
 export default function getErrorMessage(error: unknown): string {
+  // If error is not object
   if (typeof error !== 'object' || error == null) return String(error);
+  // If error has own implementation of `toString()`.
   if (Object.getOwnPropertyNames(error).includes('toString')) return error.toString();
+  // If error is simple object
   if (error.constructor === {}.constructor) return (error as Error).message || error.toString();
-  return `${error.constructor.name}: ${(error as Error).message}`;
+  // If error is instance of some class
+  return `${error.constructor.name}: ${(error as Error).message || error.toString()}`;
 }
