59ffa736e14dc482a85272d1ab943a3b071adfc0
Vladimir <vzh.inbox@gmail.com>
2021-02-17 12:25:45 +0600

    Improve getTaskCounter

--- src/getTaskCounter.ts
+++ src/getTaskCounter.ts
@@ -11,17 +11,33 @@ export interface TaskCounter<K extends string> {
   resetAll: () => void;
 }
 
-export default function getTaskCounter<K extends string = never>(
-  pendingTasks: PendingTasks<K> = { default: 0 },
-  resetTask = (tasks: typeof pendingTasks, key: keyof typeof pendingTasks) => {
+export interface TaskCounterOptions<K extends string> {
+  pendingTasks?: PendingTasks<K>;
+  resetTask?: (
+    tasks: NonNullable<this['pendingTasks']>,
+    key: keyof NonNullable<this['pendingTasks']>
+  ) => void;
+  onPending?: () => void;
+  onIdle?: () => void;
+  onStartTask?: (task: keyof NonNullable<this['pendingTasks']>) => void;
+  onEndTask?: (task: keyof NonNullable<this['pendingTasks']>) => void;
+}
+
+export default function getTaskCounter<K extends string = never>({
+  pendingTasks = { default: 0 },
+  resetTask = (tasks, key) => {
     /* eslint-disable no-param-reassign */
     if (key === 'default') {
       tasks.default = 0;
       return;
     }
     delete tasks[key];
-  }
-): TaskCounter<K> {
+  },
+  onPending,
+  onIdle,
+  onStartTask,
+  onEndTask,
+}: TaskCounterOptions<K>): TaskCounter<K> {
   const tasks = pendingTasks;
 
   const isPending = (key: keyof PendingTasks<K> = 'default'): boolean => {
@@ -36,18 +52,26 @@ export default function getTaskCounter<K extends string = never>(
 
   const push = (key: keyof PendingTasks<K> = 'default'): void => {
     // console.log('push', key);
+    const hasAny = isAnyPending();
+    const hasTask = isPending(key);
     tasks[key] = ((tasks[key] ?? 0) + 1) as PendingTasks<K>[typeof key];
+    !hasTask && onStartTask && onStartTask(key);
+    !hasAny && onPending && onPending();
   };
 
   const pop = (key: keyof PendingTasks<K> = 'default'): void => {
     // console.log('pop', key);
     if (!isPending(key)) return;
     tasks[key] = ((tasks[key] as number) - 1) as PendingTasks<K>[typeof key];
+    !isPending(key) && onEndTask && onEndTask(key);
+    !isAnyPending() && onIdle && onIdle();
   };
 
   const reset = (key: keyof PendingTasks<K> = 'default'): void => {
     if (!isPending(key)) return;
     resetTask(tasks, key);
+    onEndTask && onEndTask(key);
+    !isAnyPending() && onIdle && onIdle();
   };
 
   const resetAll = (): void => {
