2be2ff9a9cc22b35f193349e97d101b517465e74
Vladimir <vzh.inbox@gmail.com>
2021-02-17 10:27:54 +0600

    Add getTaskCounter

---
+++ src/getTaskCounter.ts
@@ -0,0 +1,65 @@
+export type PendingTasks<K extends string> = { default: number } & PartialRecord<K, number>;
+
+export interface TaskCounter<K extends string> {
+  /** true - while has at least 1 running task by key. */
+  isPending: (key?: keyof PendingTasks<K>) => boolean;
+  /** true - while has at least 1 running task. */
+  isAnyPending: () => boolean;
+  push: (key?: keyof PendingTasks<K>) => void;
+  pop: (key?: keyof PendingTasks<K>) => void;
+  reset: (key?: keyof PendingTasks<K>) => void;
+  resetAll: () => void;
+}
+
+export default function getTaskCounter<K extends string = never>(
+  pendingTasks: PendingTasks<K> = { default: 0 }
+): TaskCounter<K> {
+  const tasks = pendingTasks;
+
+  const isPending = (key: keyof PendingTasks<K> = 'default'): boolean => {
+    // console.log('calc pending', key, this.pendingTasks[key]);
+    return (tasks[key] ?? 0) > 0;
+  };
+
+  const isAnyPending = (): boolean => {
+    // console.log('calc any pending', key, this.pendingTasks[key]);
+    return Object.values(tasks).some((count) => count > 0);
+  };
+
+  const push = (key: keyof PendingTasks<K> = 'default'): void => {
+    // console.log('push', key);
+    tasks[key] = ((tasks[key] ?? 0) + 1) as PendingTasks<K>[typeof key];
+  };
+
+  const pop = (key: keyof PendingTasks<K> = 'default'): void => {
+    // console.log('pop', key);
+    if (!isPending(key)) return;
+    tasks[key] = ((tasks[key] as number) - 1) as PendingTasks<K>[typeof key];
+  };
+
+  const reset = (key: keyof PendingTasks<K> = 'default'): void => {
+    if (!isPending(key)) return;
+    if (key === 'default') {
+      tasks.default = 0;
+      return;
+    }
+    delete tasks[key];
+  };
+
+  const resetAll = (): void => {
+    if (!isAnyPending()) return;
+    Object.getOwnPropertyNames(tasks).forEach((key) => delete tasks[key]);
+    tasks.default = 0;
+  };
+
+  const counter: TaskCounter<K> = {
+    isPending,
+    isAnyPending,
+    push,
+    pop,
+    reset,
+    resetAll,
+  };
+
+  return counter;
+}
