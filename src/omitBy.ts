8fedcff39d092d17a4a85de5915fbeec0b316c4c
Vladimir <vzh.inbox@gmail.com>
2021-04-05 10:55:49 +0600

    Add omitBy

---
+++ src/omitBy.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable no-use-before-define */
+
+type OmitByPredicate<O extends AnyObject, T extends O[keyof O] = O[keyof O]> =
+  | ((value: O[typeof key], key: keyof O) => value is T)
+  | ((value: O[typeof key], key: keyof O) => boolean);
+
+export default function omitBy<O extends AnyObject, T extends O[keyof O]>(
+  obj: O,
+  predicate: OmitByPredicate<O, T>
+): O[keyof O] extends T ? Partial<O> : PartialSome<O, KeysOfType<O, T>> {
+  // ): O[keyof O] extends T ? Partial<O> : ExcludeKeysOfType<O, T> {
+  const result = {};
+  Object.getOwnPropertyNames(obj).forEach((key) => {
+    const equals = predicate(obj[key], key);
+    if (!equals) {
+      result[key] = obj[key];
+    }
+  });
+  return result as any;
+}
+
+// const o = omitBy({ a: 0, b: undefined }, (val) => val != null);
+// const o = omitBy({ a: 0, b: undefined }, (val): val is undefined => val == null);
