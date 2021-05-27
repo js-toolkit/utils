68d49e5919d02c88ff8dff540668448f018f15ee
Vladimir <vzh.inbox@gmail.com>
2021-05-27 16:16:39 +0600

    Add getTimer

---
+++ src/getTimer.ts
@@ -0,0 +1,35 @@
+export interface Timer {
+  stop: VoidFunction;
+  start: VoidFunction;
+  readonly active: boolean;
+}
+
+interface Options {
+  callback: VoidFunction;
+  interval: number;
+  autostart?: boolean;
+}
+
+export default function getTimer({ callback, interval, autostart = true }: Options): Timer {
+  let timer: any;
+
+  const stop = (): void => {
+    clearInterval(timer);
+    timer = undefined;
+  };
+
+  const start = (): void => {
+    stop();
+    timer = setInterval(callback, interval);
+  };
+
+  if (autostart) start();
+
+  return {
+    start,
+    stop,
+    get active() {
+      return timer != null;
+    },
+  };
+}
