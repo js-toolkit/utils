dda62f08b397f9097e345978dfe26622e6264dbe
Vladimir <vzh.inbox@gmail.com>
2021-06-15 10:58:51 +0600

    Add delay

---
+++ src/delay.ts
@@ -0,0 +1,33 @@
+export interface DelayFunc {
+  isPending: boolean;
+  cancel: VoidFunction;
+}
+
+export default function delay<T extends AnyFunction>(
+  fn: T,
+  wait: number,
+  ...args: Parameters<T>
+): DelayFunc {
+  let timer: any;
+
+  const cancel = (): void => {
+    clearTimeout(timer);
+    timer = undefined;
+  };
+
+  if (wait > 0) {
+    timer = setTimeout(() => {
+      timer = undefined;
+      fn.call(undefined, args);
+    }, wait);
+  } else {
+    fn.call(undefined, args);
+  }
+
+  return {
+    get isPending() {
+      return timer != null;
+    },
+    cancel,
+  };
+}
