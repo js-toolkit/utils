49b579a6ea39c73ffa42af329e1cb0be41a2bda8
Vladimir <vzh.inbox@gmail.com>
2021-07-15 13:41:38 +0600

    Add getAwaiter

---
+++ src/getAwaiter.ts
@@ -0,0 +1,84 @@
+export interface Awaiter<T> {
+  readonly pending: boolean;
+  wait: () => Promise<T>;
+  resolve: (value: T | PromiseLike<T>) => void;
+  reject: (reason?: any) => void;
+}
+
+export interface AwaiterOptions {
+  readonly lazy?: boolean;
+}
+
+export default function getAwaiter<T = void>({ lazy }: AwaiterOptions = {}): Awaiter<T> {
+  // https://stackoverflow.com/a/42118995
+
+  let pending = false;
+  let settled = false;
+  let resolveRef: Awaiter<T>['resolve'] | undefined;
+  let rejectRef: Awaiter<T>['reject'] | undefined;
+
+  let resolved: boolean;
+  let resolveValue: T | PromiseLike<T>;
+  let rejected: boolean;
+  let rejectValue: unknown;
+
+  let promise: Promise<T>;
+
+  const wait = (): Promise<T> => {
+    if (promise == null) {
+      promise = new Promise<T>((resolve, reject) => {
+        pending = true;
+        resolveRef = resolve;
+        rejectRef = reject;
+        if (resolved) resolve(resolveValue);
+        if (rejected) reject(rejectValue);
+      }).finally(() => {
+        // Changed only after all chain will resolved.
+        pending = false;
+        settled = true;
+      });
+    }
+    return promise;
+  };
+
+  if (!lazy) {
+    void wait();
+  }
+
+  return {
+    // promise,
+    get pending() {
+      return pending;
+    },
+    wait,
+    resolve: (value) => {
+      if (!pending && !settled) {
+        resolved = true;
+        resolveValue = value;
+        rejected = false;
+      }
+      resolveRef && resolveRef(value);
+    },
+    reject: (error) => {
+      if (!pending && !settled) {
+        rejected = true;
+        rejectValue = error;
+        resolved = false;
+      }
+      rejectRef && rejectRef(error);
+    },
+  };
+}
+
+// const w = getAwaiter<number>();
+
+// setTimeout(() => {
+//   console.log(w.pending);
+//   w.resolve(10);
+//   console.log(w.pending);
+// }, 2000);
+
+// void (async () => {
+//   const result = await w.wait().then(() => new Promise((resolve) => setTimeout(resolve, 1000)));
+//   console.log('done', result, w.pending);
+// })();
