c6fb09f52ac0e9af46a6bd5ee854918f8aff8c8a
Vladimir <vzh.inbox@gmail.com>
2021-07-22 21:40:26 +0600

    Improve

--- src/farey.ts
+++ src/farey.ts
@@ -1,19 +1,22 @@
 // https://code.activestate.com/recipes/52317/
 // https://stackoverflow.com/a/43016456
 
-/** Converts a numeric to a rational.
- * @returns [numerator, denominator], [1,0] is infinity. */
+/**
+ * Rational approximation algorithm with adjustable level of fuzziness.
+ * Converts a numeric to a rational.
+ * @returns [numerator, denominator], [1,0] is infinity.
+ */
 export default function farey(
   value: number,
   /** Max denominator */
   limit: number
-): [numerator: number, denominator: number] {
-  let lower: [number, number] = [0, 1];
-  let upper: [number, number] = [1, 0];
+): readonly [numerator: number, denominator: number] {
+  let lower: readonly [number, number] = [0, 1];
+  let upper: readonly [number, number] = [1, 0];
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    const mediant: [number, number] = [lower[0] + upper[0], lower[1] + upper[1]];
+    const mediant: readonly [number, number] = [lower[0] + upper[0], lower[1] + upper[1]];
 
     if (value * mediant[1] > mediant[0]) {
       if (limit < mediant[1]) return upper;
@@ -30,3 +33,4 @@ export default function farey(
 }
 
 // console.log(farey(Math.PI, 50));
+// console.log(farey(1035 / 582, 50));
