75db17d522b006eae8402db8cc114c76396d0daa
Vladimir <vzh.inbox@gmail.com>
2021-12-13 10:48:28 +0600

    Refactor

--- src/copyFnProps.ts
+++ src/copyFnProps.ts
@@ -1,10 +1,24 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
 export default function copyFnProps<T extends AnyFunction>(source: T, dest: T): T {
-  const descs = Object.getOwnPropertyDescriptors(source);
+  // Do not use `Object.getOwnPropertyDescriptors` for ES5 capability
+  const descs = Object.getOwnPropertyNames(source).reduce((acc, prop) => {
+    const desc = Object.getOwnPropertyDescriptor(source, prop);
+    desc && (acc[prop] = desc);
+    return acc;
+  }, {} as Partial<Record<keyof Function, PropertyDescriptor>>);
+
   delete descs.arguments;
   delete descs.caller;
   delete descs.length;
   delete descs.name;
   delete descs.prototype;
+  delete descs.apply;
+  delete descs.call;
+  delete descs.bind;
+  delete descs.toString;
+
   Object.defineProperties(dest, descs);
+
   return dest;
 }
