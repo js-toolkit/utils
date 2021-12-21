c960ff313f36c4912ae995df74183376f50ff47c
Vladimir <vzh.inbox@gmail.com>
2021-12-21 08:43:31 +0600

    Add once

--- src/memoizeAsync.ts
+++ src/memoizeAsync.ts
@@ -2,15 +2,25 @@ type MemoizedAsync<T extends AnyAsyncFunction> = T & {
   readonly promise: ReturnType<T> | undefined;
 };
 
+interface MemoizeAsyncOptions {
+  readonly once?: boolean;
+}
+
 export function memoizeAsync<T extends AnyAsyncFunction>(
   func: T,
+  options?: MemoizeAsyncOptions,
   ...rest: Parameters<ReturnType<T>['then']>
 ): MemoizedAsync<T> {
-  let callPromise: Promise<unknown>;
+  let callPromise: Promise<unknown> | undefined;
 
   const memoized = ((...args: unknown[]) => {
     if (callPromise == null) {
       callPromise = func(...args).then(...rest);
+      if (options?.once) {
+        callPromise.finally(() => {
+          callPromise = undefined;
+        });
+      }
     }
     return callPromise;
   }) as MemoizedAsync<T>;
