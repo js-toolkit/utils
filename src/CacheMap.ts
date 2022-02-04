90449de354ac5c5c44f1000dad5dad282f5fb92c
Vladimir <vzh.inbox@gmail.com>
2022-02-04 09:15:44 +0600

    Improve CacheMap

--- src/CacheMap.ts
+++ src/CacheMap.ts
@@ -1,11 +1,29 @@
 export default class CacheMap<K, V> extends Map<K, V> {
+  private readonly queue = new Map<K, Promise<unknown>>();
+
+  getOrQueue<F extends () => V | Promise<V>>(
+    key: K,
+    factory: F
+  ): Awaited<ReturnType<F>> | undefined {
+    if (!this.has(key) && !this.queue.has(key)) {
+      this.queue.set(
+        key,
+        Promise.resolve()
+          .then(() => factory())
+          .then((val) => this.set(key, val))
+          .finally(() => this.queue.delete(key))
+      );
+    }
+    return this.get(key) as Awaited<ReturnType<F>>;
+  }
+
   async getOrCreate<F extends () => V | Promise<V>>(
     key: K,
     factory: F
   ): Promise<Awaited<ReturnType<F>>> {
-    if (!this.has(key) && factory) {
-      const val = await factory();
-      this.set(key, val);
+    await this.queue.get(key);
+    if (!this.has(key)) {
+      this.set(key, await factory());
     }
     return this.get(key) as Awaited<ReturnType<F>>;
   }
