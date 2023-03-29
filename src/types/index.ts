226ea9247aa716993d8b46d62d03a45c29fdb0ec
Vladimir <vzh.inbox@gmail.com>
2023-03-29 12:13:11 +0600

    Update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -123,6 +123,8 @@ type OmitIndex<T extends AnyObject> = {
   [P in keyof T as string extends P ? never : number extends P ? never : P]: T[P];
 };
 
+type WithIndex<T extends AnyObject> = T & Record<string, any>;
+
 // type KeepTypes<A extends AnyObject, B, K extends keyof A = keyof A> = ExcludeKeysOfType<
 // { [P in keyof A]: P extends K ? Extract<A[P], B> : A[P] },
 // never | undefined
