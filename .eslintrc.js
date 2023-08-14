55bf88b735444e28a445331582d078609c053e8d
Vladimir <vzh.inbox@gmail.com>
2023-08-14 17:29:09 +0600

    Update imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: require.resolve('@jstoolkit/configs/eslint/common'),
+  extends: require.resolve('@js-toolkit/configs/eslint/common'),
   rules: {
     'import/export': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
