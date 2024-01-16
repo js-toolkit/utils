14584c84df4ebe3c133d33a275a32b2acc929e83
Vladimir <vzh.inbox@gmail.com>
2024-01-16 11:31:23 +0600

    Improve getErrorMessage

--- src/getErrorMessage.ts
+++ src/getErrorMessage.ts
@@ -1,14 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-base-to-string */
+
 function objectToString(error: AnyObject): string {
   const msg = error.message as string;
   if (msg) return msg;
 
-  const errStr = error.toString();
+  const errStr = (error as Object).toString();
 
   if (errStr === {}.toString()) {
     try {
       return JSON.stringify(error);
     } catch {
-      console.log(error);
+      console.warn('Stringify object failed:', error);
     }
   }
 
@@ -19,7 +22,7 @@ export function getErrorMessage(error: unknown): string {
   // If error is not object
   if (typeof error !== 'object' || error == null) return String(error);
   // If error has own implementation of `toString()`.
-  if (Object.getOwnPropertyNames(error).includes('toString')) return error.toString();
+  if (Object.hasOwn(error, 'toString')) return (error as Object).toString();
   // If error is simple object
   if (error.constructor === {}.constructor) return objectToString(error);
   // If error is instance of some class
