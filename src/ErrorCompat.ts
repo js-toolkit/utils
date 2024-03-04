8664d4723779ac2c3f5e49f44acf9adce75c2535
Vladimir <vzh.inbox@gmail.com>
2024-03-04 14:26:50 +0500

    Improve ErrorCompat

--- src/ErrorCompat.ts
+++ src/ErrorCompat.ts
@@ -1,12 +1,19 @@
 import { es5ErrorCompat } from './es5ErrorCompat';
 
+export interface ErrorCompatOptions extends ErrorOptions {
+  readonly name?: string | undefined;
+}
+
 export abstract class ErrorCompat extends Error {
   constructor(
     SelfConstructor: AnyConstructor,
     message?: string | undefined,
-    options?: ErrorOptions | undefined
+    options?: ErrorCompatOptions | undefined
   ) {
     super(message, options);
     es5ErrorCompat(this, SelfConstructor);
+    if (options?.name != null) {
+      this.name = options.name;
+    }
   }
 }
