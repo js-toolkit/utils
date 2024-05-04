91e953362ac215669708d3c529c8d053944fdf5c
Vladimir <vzh.inbox@gmail.com>
2024-05-04 11:09:21 +0500

    Improve clear

--- src/clear.ts
+++ src/clear.ts
@@ -1,11 +1,14 @@
+/* eslint-disable no-param-reassign */
 type ClearResult<T extends AnyObject> = T extends ReadonlyArray<unknown> ? T : Partial<T>;
 
 export function clear<T extends AnyObject>(obj: T): ClearResult<T> {
   if (Array.isArray(obj)) {
-    obj.length > 0 && obj.splice(0, obj.length);
+    // obj.length > 0 && obj.splice(0, obj.length);
+    if (obj.length > 0) {
+      obj.length = 0;
+    }
   } else {
     Object.getOwnPropertyNames(obj).forEach((key) => {
-      // eslint-disable-next-line no-param-reassign
       delete obj[key];
     });
   }
