a6758ddbdcd2254767c276f82fae6130845438ee
Vladimir <vzh.inbox@gmail.com>
2024-07-29 22:41:19 +0500

    Add hasOwn

---
+++ src/hasOwn.ts
@@ -0,0 +1,7 @@
+export function hasOwn<
+  T extends AnyObject,
+  K extends T extends T ? (T extends ArrayLike<any> ? number : keyof T) : never,
+>(obj: T, prop: K): obj is T extends T ? (K extends keyof T ? T : never) : never {
+  // ): obj is T extends T ? (K extends keyof T ? T : T extends ArrayLike<any> ? never : never) : never {
+  return Object.hasOwn(obj, prop);
+}
