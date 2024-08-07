fe9832e7e82e345d144ea0d8253eba159be5a904
Vladimir <vzh.inbox@gmail.com>
2024-08-07 14:35:14 +0500

    Improve isEmptyObject

--- src/isEmptyObject.ts
+++ src/isEmptyObject.ts
@@ -2,8 +2,8 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable guard-for-in */
 
-export function isEmptyObject<T extends AnyObject>(obj: T): boolean {
-  if (Array.isArray(obj)) {
+export function isEmptyObject<T extends AnyObject | string>(obj: T): boolean {
+  if (Array.isArray(obj) || typeof obj === 'string') {
     return obj.length === 0;
   }
   for (const _ in obj) {
