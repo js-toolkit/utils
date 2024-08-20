78ebee980c44a3602a6ff8940887295d3d128068
Vladimir <vzh.inbox@gmail.com>
2024-08-20 11:56:29 +0500

    Add jsdoc

--- src/isEmptyObject.ts
+++ src/isEmptyObject.ts
@@ -2,6 +2,9 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable guard-for-in */
 
+/**
+ * @param obj Can be `Array | string | object`.
+ */
 export function isEmptyObject<T extends AnyObject | string>(obj: T): boolean {
   if (Array.isArray(obj) || typeof obj === 'string') {
     return obj.length === 0;
