dfb11ecedaece7c0a0ce5f8bd1a877317a4fe381
Vladimir <vzh.inbox@gmail.com>
2024-12-05 11:23:04 +0500

    Update hasIn

--- src/hasIn.ts
+++ src/hasIn.ts
@@ -1,6 +1,7 @@
 export function hasIn<
   T extends AnyObject,
-  K extends T extends T ? (T extends ArrayLike<any> ? number : keyof T) : never,
+  // K extends T extends T ? (T extends ArrayLike<any> ? number : keyof T) : never,
+  K extends Keys<T>,
 >(obj: T, prop: K): obj is T extends T ? (K extends keyof T ? T : never) : never {
   // ): obj is T extends T ? (K extends keyof T ? T : T extends ArrayLike<any> ? never : never) : never {
   return prop in obj;
