267bff62e8e68c2141fc4ad3e83ee434be8664a9
Vladimir <vzh.inbox@gmail.com>
2025-02-06 19:20:02 +0500

    Improve ConsolePlugin

--- src/log/ConsolePlugin.ts
+++ src/log/ConsolePlugin.ts
@@ -2,7 +2,18 @@
 /* eslint-disable class-methods-use-this */
 import { hasIn } from '../hasIn';
 import { Plugin } from './Plugin';
-import type log from './log';
+import log from './log';
+
+// const levelMethodMap: Record<log.Level, keyof Console | 'none'> = {
+//   none: 'none',
+//   error: 'error',
+//   warn: 'warn',
+//   info: 'info',
+//   debug: 'debug',
+//   v1: 'debug',
+//   v2: 'debug',
+//   trace: 'trace',
+// };
 
 // Build the best logging method possible for this env
 // Wherever possible we want to bind, not wrap, to preserve stack traces
@@ -12,15 +23,18 @@ export class ConsolePlugin extends Plugin {
   override notifyOfChange(): void {}
 
   override factory(_logger: log.Logger, level: log.Level): log.LoggingMethod | undefined {
-    if (typeof console === 'undefined') {
+    if (typeof console === 'undefined' || level === 'none') {
       return undefined;
     }
+    // const method = levelMethodMap[level];
     const method = level as keyof Console;
     const methodName =
-      hasIn(console, method) && typeof console[method] === 'function' ? method : undefined;
-    if (methodName) {
-      return (console[methodName] as log.LoggingMethod).bind(console);
-    }
-    return undefined;
+      hasIn(console, method) && typeof console[method] === 'function' ? method : 'debug';
+    return (console[methodName] as log.LoggingMethod).bind(console);
+    // if (methodName) {
+    //   return (console[methodName] as log.LoggingMethod).bind(console);
+    // }
+    // console.warn(`Unknown method '${methodName}'.`);
+    // return undefined;
   }
 }
