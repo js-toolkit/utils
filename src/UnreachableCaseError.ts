355d9ca3d787e82239448f8ae1f4526d743ba7d3
Vladimir <vzh.inbox@gmail.com>
2025-09-04 11:15:12 +0500

    Add message param

--- src/UnreachableCaseError.ts
+++ src/UnreachableCaseError.ts
@@ -1,8 +1,8 @@
 import { es5ErrorCompat } from './es5ErrorCompat';
 
 export class UnreachableCaseError extends Error {
-  constructor(value: never) {
-    super(`Unreachable case: ${String(value)}`);
+  constructor(value: never, message?: string) {
+    super(message ?? `Unreachable case: ${String(value)}`);
     es5ErrorCompat(this, UnreachableCaseError);
   }
 }
