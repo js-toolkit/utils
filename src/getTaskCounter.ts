6e7d8a9e82e911787789eca2387bae53dedde1d8
Vladimir <vzh.inbox@gmail.com>
2026-03-15 21:00:24 +0500

    Refactor to use Map

--- src/getTaskCounter.ts
+++ src/getTaskCounter.ts
@@ -13,72 +13,73 @@ export interface TaskCounter<K extends string> {
 
 export interface TaskCounterOptions<K extends string> {
   pendingTasks?: PendingTasks<K> | undefined;
-  resetTask?:
-    | ((
-        tasks: NonNullable<this['pendingTasks']>,
-        key: keyof NonNullable<this['pendingTasks']>
-      ) => void)
-    | undefined;
+  resetTask?: ((tasks: Map<typeof key, number>, key: keyof PendingTasks<K>) => void) | undefined;
   onPending?: VoidFunction | undefined;
   onIdle?: VoidFunction | undefined;
-  onStartTask?: ((task: keyof NonNullable<this['pendingTasks']>) => void) | undefined;
-  onEndTask?: ((task: keyof NonNullable<this['pendingTasks']>) => void) | undefined;
+  onStartTask?: ((task: keyof PendingTasks<K>) => void) | undefined;
+  onEndTask?: ((task: keyof PendingTasks<K>) => void) | undefined;
+}
+
+function defaultResetTask<K extends string>(
+  tasks: Map<typeof key, number>,
+  key: keyof PendingTasks<K>
+): void {
+  if (key === 'default') {
+    tasks.set('default', 0);
+    return;
+  }
+  tasks.delete(key);
 }
 
 export function getTaskCounter<K extends string = never>({
   pendingTasks = { default: 0 },
-  resetTask = (tasks, key) => {
-    if (key === 'default') {
-      tasks.default = 0;
-      return;
-    }
-    delete tasks[key];
-  },
+  resetTask = defaultResetTask<K>,
   onPending,
   onIdle,
   onStartTask,
   onEndTask,
 }: TaskCounterOptions<K>): TaskCounter<K> {
-  const tasks = pendingTasks;
+  const tasks = new Map<keyof PendingTasks<K>, number>(
+    Object.entries(pendingTasks) as [keyof PendingTasks<K>, number][]
+  );
 
   const isPending = (key: keyof PendingTasks<K> = 'default'): boolean => {
     // console.log('calc pending', key, this.pendingTasks[key]);
-    return (tasks[key] ?? 0) > 0;
+    return (tasks.get(key) ?? 0) > 0;
   };
 
   const isAnyPending = (): boolean => {
     // console.log('calc any pending', key, this.pendingTasks[key]);
-    return Object.values(tasks).some((count) => count > 0);
+    return tasks.values().some((count) => count > 0);
   };
 
   const push = (key: keyof PendingTasks<K> = 'default'): void => {
     // console.log('push', key);
     const hasAny = isAnyPending();
     const hasTask = isPending(key);
-    tasks[key] = ((tasks[key] ?? 0) + 1) as PendingTasks<K>[typeof key];
-    !hasTask && onStartTask && onStartTask(key);
-    !hasAny && onPending && onPending();
+    tasks.set(key, (tasks.get(key) ?? 0) + 1);
+    !hasTask && onStartTask?.(key);
+    !hasAny && onPending?.();
   };
 
   const pop = (key: keyof PendingTasks<K> = 'default'): void => {
     // console.log('pop', key);
     if (!isPending(key)) return;
-    tasks[key] = ((tasks[key] as number) - 1) as PendingTasks<K>[typeof key];
-    !isPending(key) && onEndTask && onEndTask(key);
-    !isAnyPending() && onIdle && onIdle();
+    tasks.set(key, (tasks.get(key) ?? 1) - 1);
+    !isPending(key) && onEndTask?.(key);
+    !isAnyPending() && onIdle?.();
   };
 
   const reset = (key: keyof PendingTasks<K> = 'default'): void => {
     if (!isPending(key)) return;
     resetTask(tasks, key);
-    onEndTask && onEndTask(key);
-    !isAnyPending() && onIdle && onIdle();
+    onEndTask?.(key);
+    !isAnyPending() && onIdle?.();
   };
 
   const resetAll = (): void => {
     if (!isAnyPending()) return;
-    const keys = Object.getOwnPropertyNames(tasks) as (keyof PendingTasks<K>)[];
-    keys.forEach(reset);
+    tasks.keys().forEach(reset);
   };
 
   const counter: TaskCounter<K> = {
